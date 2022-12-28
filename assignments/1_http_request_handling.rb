ef7300727085e8010beb475569b3d3723969cf3e
Yubee116 <ubongnehemiah@gmail.com>
2022-12-28 19:37:29 +0100

    swim again

--- assignments/1_http_request_handling.rb
+++ assignments/1_http_request_handling.rb
@@ -1,17 +1,37 @@
 require 'net/http'
 require 'json'
 
-uri = URI('https://rickandmortyapi.com/api/episode')
-res = Net::HTTP.get_response(uri)
+episodes = []
+characters_store = {}
 
-episodes = JSON.parse(res.body)['results'] if res.is_a?(Net::HTTPSuccess)
+rick_and_morty_host = URI('https://rickandmortyapi.com/api/').host
 
-episodes.each do |episode|
-  character_urls = episode['characters']
+Net::HTTP.start(rick_and_morty_host, use_ssl: true) do |http|
+  episodes_uri = URI('https://rickandmortyapi.com/api/episode')
+  until episodes_uri.nil?
+    episodes_request = Net::HTTP::Get.new episodes_uri
+    episodes_response = http.request episodes_request
+    if episodes_response.is_a?(Net::HTTPSuccess)
+      (JSON.parse(episodes_response.body)['results']).each do |episode_obj|
+        episodes.append(episode_obj)
+      end
+    end
+    next_page = JSON.parse(episodes_response.body)['info']['next']
+    episodes_uri = next_page.nil? ? nil : URI(next_page)
+  end
 
-  character_urls.each_with_index do |character_url, index|
-    character_object = Net::HTTP.get_response(URI(character_url))
-    episode['characters'][index] = character_object.body if character_object.is_a?(Net::HTTPSuccess)
+  episodes.each do |episode|
+    character_urls = episode['characters']
+
+    character_urls.each_with_index do |character_url, index|
+      unless characters_store.has_key?(character_url)
+        character_request = Net::HTTP::Get.new URI(character_url)
+        character_object = http.request character_request
+        characters_store[character_url] = character_object.body if character_object.is_a?(Net::HTTPSuccess)
+      end
+
+      episode['characters'][index] = characters_store[character_url]
+    end
   end
 end
 
--- for_bonus/spec/counter_spec.rb
+++ for_bonus/spec/counter_spec.rb
@@ -17,7 +17,7 @@ describe Counter do
     end
   end
 
-  context 'given initial value - 7' do
+  context 'given initial value: 7' do
     context 'and the first method is called' do
       it 'returns 7 (inital value)' do
         expect(Counter.counter(7)[0].call).to eq(7)
@@ -30,10 +30,82 @@ describe Counter do
       end
     end
   end
+
+  context 'given negative initial value: -7' do
+    context 'and the first method is called' do
+      it 'returns -7 (inital value)' do
+        expect(Counter.counter(-7)[0].call).to eq(-7)
+      end
+    end
+
+    context 'and the second method is called' do
+      it 'returns -6 (inital value plus one)' do
+        expect(Counter.counter(-7)[1].call).to eq(-6)
+      end
+    end
+  end
   
   context 'given non-number' do
-    it 'returns nil' do
-      expect(Counter.counter('10')).to eq(nil)
+    context 'given a string' do
+      it 'returns nil' do
+        expect(Counter.counter('10')).to eq(nil)
+      end
+    end
+
+    context 'given an array' do
+      it 'returns nil' do
+        expect(Counter.counter(['a','b','c'])).to eq(nil)
+      end
+    end
+  end
+
+  context 'given infinite variable' do
+    context 'given positive infinity' do
+      it 'returns positive infinity' do
+        expect(Counter.counter(Float::INFINITY)[0].call).to eq(Float::INFINITY)
+      end
+    end
+
+    context 'given negative infinity' do
+      it 'returns negative' do
+        expect(Counter.counter(-Float::INFINITY)[0].call).to eq(-Float::INFINITY)
+      end
+    end
+  end
+
+  context 'called three times with different positive initial values: 5, 10 and 15' do
+    context 'and the first method is called' do
+      it 'returns 5, 10, 15 (the independent counter inital values)' do
+        expect(Counter.counter(5)[0].call).to eq(5)
+        expect(Counter.counter(10)[0].call).to eq(10)
+        expect(Counter.counter(15)[0].call).to eq(15)
+      end
+    end
+
+    context 'and the second method is called' do
+      it 'returns 6 and 11 (the different counter initial values plus one)' do
+        expect(Counter.counter(5)[1].call).to eq(6)
+        expect(Counter.counter(10)[1].call).to eq(11)
+        expect(Counter.counter(15)[1].call).to eq(16)
+      end
+    end
+  end
+
+  context 'called three times with different negative initial values: -5, -10 and -15' do
+    context 'and the first method is called' do
+      it 'returns -5, -10, -15 (the independent counter inital values)' do
+        expect(Counter.counter(-5)[0].call).to eq(-5)
+        expect(Counter.counter(-10)[0].call).to eq(-10)
+        expect(Counter.counter(-15)[0].call).to eq(-15)
+      end
+    end
+
+    context 'and the second method is called' do
+      it 'returns -4, -9, -14 (the independent counter inital values plus one)' do
+        expect(Counter.counter(-5)[1].call).to eq(-4)
+        expect(Counter.counter(-10)[1].call).to eq(-9)
+        expect(Counter.counter(-15)[1].call).to eq(-14)
+      end
     end
   end
 end
