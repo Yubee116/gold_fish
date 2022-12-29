b3efd3de7a58c07d71aa665960063220d9fc18bb
Yubee116 <ubongnehemiah@gmail.com>
2022-12-29 09:10:32 +0100

    swims in self disdain

--- assignments/2_counter_function.rb
+++ assignments/2_counter_function.rb
@@ -5,7 +5,7 @@ def counter(starting_value = 0, *_args)
   [-> { counter }, -> { counter += 1 }]
 end
 
-# yubees_counter = Counter.counter(30)
+# yubees_counter = counter(30)
 
 # puts "Current value of counter:  #{yubees_counter[0].call}"
 # puts "Increment current value of counter and print new value:  #{yubees_counter[1].call}"
